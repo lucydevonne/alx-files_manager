33065ee594c365d26a95531a01789c692aeaa71b
root <wambuilucie7@gmail.com>
2023-10-06 13:47:56 -0700

    file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,28 +1,19 @@
-// controllers/AppController.js
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-const redisClient = require('../utils/redis');
-const db = require('../utils/db');
-
-exports.getStatus = (req, res) => {
-  // Check Redis and DB status
-  const redisStatus = redisClient.connected;
-  const dbStatus = !db.readyState || db.readyState === 1;
-
-  res.status(200).json({ redis: redisStatus, db: dbStatus });
-};
-
-exports.getStats = async (req, res) => {
-  try {
-    // You should replace 'User' and 'File' with your actual Mongoose models
-    const User = require('../models/User');
-    const File = require('../models/File');
-
-    const userCount = await User.countDocuments();
-    const fileCount = await File.countDocuments();
-
-    res.status(200).json({ users: userCount, files: fileCount });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
   }
-};
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
